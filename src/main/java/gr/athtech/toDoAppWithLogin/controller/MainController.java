cb0f62855c9921b71c3f58b53146dfb4faeeed28
John Daniil <jdaniil@24media.gr>
2020-11-16 14:45:56 +0200

    added ability to add and delete item

--- src/main/java/gr/athtech/toDoAppWithLogin/controller/MainController.java
+++ src/main/java/gr/athtech/toDoAppWithLogin/controller/MainController.java
@@ -1,22 +1,32 @@
 package gr.athtech.toDoAppWithLogin.controller;
 
 import gr.athtech.toDoAppWithLogin.ToDoAppWithLoginApplication;
+import gr.athtech.toDoAppWithLogin.model.Item;
 import gr.athtech.toDoAppWithLogin.service.InitiationService;
+import gr.athtech.toDoAppWithLogin.service.ItemService;
 import gr.athtech.toDoAppWithLogin.service.SessionService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
     SessionService sessionService;
     InitiationService initiationService;
+    ItemService itemService;
 
 
-    public MainController(SessionService sessionService, InitiationService initiationService) {
+    public MainController(SessionService sessionService, InitiationService initiationService, ItemService itemService) {
         this.sessionService = sessionService;
         this.initiationService = initiationService;
+        this.itemService = itemService;
     }
 
     @GetMapping("/login")
@@ -38,7 +48,21 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String main() {
-            return "main";
+    public String main(Model model) {
+        List<Item> allItems = itemService.findAllItems();
+        model.addAttribute("items", allItems);
+        return "main";
+    }
+
+    @PostMapping("/item/add")
+    public String addItem(@RequestParam String description) {
+        itemService.addItem(description);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/item/delete")
+    public String deleteItem(@RequestParam long id) {
+        itemService.deleteItem(id);
+        return "redirect:/";
     }
 }
--- src/main/java/gr/athtech/toDoAppWithLogin/model/Item.java
+++ src/main/java/gr/athtech/toDoAppWithLogin/model/Item.java
@@ -1,4 +1,28 @@
-package gr.athtech.toDoAppWithLogin.model.security;
+package gr.athtech.toDoAppWithLogin.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private Date date_created;
+
+    public Item(String username) {
+        this.username = username;
+        date_created = new Date();
+    }
 }
